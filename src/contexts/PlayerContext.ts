4ab55e59ea2df727ed06c4bdbeeea1b369d9367e
Wesley Anjos <wanjos.andrade@gmail.com>
2021-04-22 11:51:27 -0300

    Implemented play and pause function

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -11,7 +11,10 @@ type Episode = {
 type PlayerContextData = {
   episodesList: Episode[];
   currentEpisodeIndex: number;
+  isPlaying: boolean;
   play: (episode: Episode) => void;
+  setPlayingState: (state: boolean) => void;
+  togglePlay: () => void;
 }
 
 export const PlayerContext = createContext({}as PlayerContextData)
