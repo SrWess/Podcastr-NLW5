9ac6ae24344070e7eaeaf211623455070a4d499d
Wesley Anjos <wanjos.andrade@gmail.com>
2021-04-24 12:28:34 -0300

    Current time control

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { usePlayer } from "../../contexts/PlayerContext";
 import Slider from "rc-slider";
 
 import "rc-slider/assets/index.css";
 import styles from "./styles.module.scss";
+import convertDurationToTimeString from "../../utils/convertDurationToTimeString";
 
 export default function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [progress, setProgress] = useState(0)
 
   const {
     episodesList,
@@ -37,6 +39,14 @@ export default function Player() {
     }
   }, [isPlaying]);
 
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0
+
+    audioRef.current.addEventListener('timeupdate', () => {
+      setProgress(Math.floor(audioRef.current.currentTime))
+    })
+  }
+
   const episode = episodesList[currentEpisodeIndex];
 
   return (
@@ -65,10 +75,12 @@ export default function Player() {
 
       <footer className={!episode ? styles.empty : ""}>
         <div className={styles.progress}>
-          <span>00:00</span>
+        <span>{ convertDurationToTimeString(progress) }</span>
           <div className={styles.slider}>
             {episode ? (
               <Slider
+                max={episode.duration}
+                value={progress}
                 trackStyle={{ backgroundColor: "#04D361 " }}
                 railStyle={{ backgroundColor: "#9f75ff" }}
                 handleStyle={{ borderColor: "#04D361", borderWidth: 4 }}
@@ -77,7 +89,7 @@ export default function Player() {
               <div className={styles.emptySlider} />
             )}
           </div>
-          <span>00:00</span>
+          <span>{ convertDurationToTimeString(episode?.duration ?? 0) }</span>
         </div>
 
         {episode && (
@@ -88,6 +100,7 @@ export default function Player() {
             loop={isLooping}
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
+            onLoadedMetadata={setupProgressListener}
           />
         )}
 
